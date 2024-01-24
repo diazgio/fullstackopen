98fdbdff7a399dd83a2fad789a0d352b417a73c6
Giordano Martín Díaz Merino <diazgiordano@gmail.com>
2024-01-23 20:01:14 -0500

    Finish part 1.3 - 1.5

--- part1/courseinfo/src/Content.jsx
+++ part1/courseinfo/src/Content.jsx
@@ -2,13 +2,21 @@ import React from 'react';
 import Part from './Part';
 
 const Content = (props) => {
-    const { parts, exercises } = props;
+    const [ ...parts ]   = props.parts;
 
     return (
         <>
-          <Part name={parts[0]} exercises={exercises[0]} />
-          <Part name={parts[1]} exercises={exercises[1]} />
-          <Part name={parts[2]} exercises={exercises[2]} />
+          {parts.map((part, index) => {
+              return (
+                  <Part
+                    key={index}
+                    name={part.name}
+                    exercises={part.exercises}
+                  />
+              )
+              }
+            )
+          }
         </>
     );
 };
--- part1/courseinfo/src/Total.jsx
+++ part1/courseinfo/src/Total.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 
-const Total = ({ numbers }) => {
-    const sum = numbers.reduce((acc, curr) => acc + curr, 0);
-
+const Total = (props) => {
+    const [ ...parts ]   = props.numbers;
+    const sum = parts.reduce((accumulator, currentValue) => {
+        return accumulator + currentValue.exercises;
+    }, 0);
     return (
         <>
-            <h2>Total</h2>
-            <p>Number of exercises: {sum}</p>
+          <h2>Total</h2>
+          <p>Number of exercises: {sum}</p>
         </>
     );
 };
