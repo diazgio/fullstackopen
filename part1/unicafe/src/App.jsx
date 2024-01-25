246ec854c19a908a182d0fd7a1d3cf3c1da7c814
Giordano Martín Díaz Merino <diazgiordano@gmail.com>
2024-01-25 18:19:44 -0500

    Finish part 1.7

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -5,20 +5,34 @@ import Display from './Display';
 
 const App = () => {
   // guarda los clics de cada botón en su propio estado
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+  const [all, setAll] = useState([]);
+  const goodPoints = 1;
+  const neutralPoints = 0;
+  const badPoints = -1; 
 
   const handleGoodClick = () => {
-    setGood(good + 1)
+    setAll(all.concat('G'));
+    setGood(good + 1);
   }
 
   const handleNeutralClick = () => {
-    setNeutral(neutral + 1)
+    setAll(all.concat('N'));
+    setNeutral(neutral + 1);
   }
 
   const handleBadClick = () => {
-    setBad(bad + 1)
+    setAll(all.concat('B'));
+    setBad(bad + 1);
+  }
+
+  let average = 0;
+  let positive = 0;
+  if (all.length > 0) {
+    average = ((parseInt(good) * goodPoints) + (parseInt(neutral) * neutralPoints) + (parseInt(bad) * badPoints)) / 3;
+    positive = good / all.length * 100;
   }
 
   return (
@@ -29,11 +43,14 @@ const App = () => {
         <Button onClick={handleNeutralClick} text="Neutral" />
         <Button onClick={handleBadClick} text="Bad" />
       </div>
-      <h2>Statics</h2>
+      <h2>Statistics</h2>
       <div>
         <Display text="Good" value={good} />
         <Display text="Neutral" value={neutral} />
         <Display text="Bad" value={bad} />
+        <Display text="All" value={all.length} />
+        <Display text="Average" value={average} />
+        <Display text="Positive" value={positive + ' %'} />
       </div>
     </div>
   )
