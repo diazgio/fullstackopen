3d7a479e820c4e5bc5f5707e4847f651d639ef90
Giordano Martín Díaz Merino <diazgiordano@gmail.com>
2024-01-26 14:24:28 -0500

    Finish part 2.6, part 2.5 was solved from the begining

--- part2/the_phone_book/src/App.jsx
+++ part2/the_phone_book/src/App.jsx
@@ -1,4 +1,5 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
+import Name from './components/Name';
 
 const App = () => {
   const [ persons, setPersons ] = useState([
@@ -6,19 +7,32 @@ const App = () => {
   ]) 
   const [ newName, setNewName ] = useState('')
 
+  const handleNameChange = (event) => {
+    setNewName(event.target.value);
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const newPerson = { name: newName };
+    setPersons([...persons, newPerson]);
+    setNewName('');
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={handleSubmit}>
         <div>
-          name: <input />
+          name: <input value={newName} onChange={handleNameChange} />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      {persons.map((person) => (
+        <Name key={person.name} name={person.name} />
+      ))}
     </div>
   )
 }
