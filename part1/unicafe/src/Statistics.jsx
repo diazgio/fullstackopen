3d81c327009495af6073ffee0325d91957d2c316
Giordano Martín Díaz Merino <diazgiordano@gmail.com>
2024-01-25 19:04:32 -0500

    Finish part 1.9

--- part1/unicafe/src/Statistics.jsx
+++ part1/unicafe/src/Statistics.jsx
@@ -3,17 +3,26 @@ import Display from './Display';
 
 const Statistics = (props) => {
   const { good, neutral, bad, all, average, positive } = props;
-  return (
-    <>
-      <h2>Statistics</h2>
-      <Display text="Good" value={good} />
-      <Display text="Neutral" value={neutral} />
-      <Display text="Bad" value={bad} />
-      <Display text="All" value={all.length} />
-      <Display text="Average" value={average} />
-      <Display text="Positive" value={positive + ' %'} />
-    </>
-  );
+  if (all.length === 0) {
+    return (
+      <>
+        <h2>Statistics</h2>
+        <p>No feedback given</p>
+      </>
+    );
+  }else {
+    return (
+      <>
+        <h2>Statistics</h2>
+        <Display text="Good" value={good} />
+        <Display text="Neutral" value={neutral} />
+        <Display text="Bad" value={bad} />
+        <Display text="All" value={all.length} />
+        <Display text="Average" value={average} />
+        <Display text="Positive" value={positive + ' %'} />
+      </>
+    );  
+  }
 };
 
 export default Statistics;
